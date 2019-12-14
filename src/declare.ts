55d1211878b8169a5d8c0d35fc5afa55966a2970
WMXPY <wm@mengw.io>
2019-12-14 13:22:16 -0600

    extract common functions

--- src/declare.ts
+++ src/declare.ts
@@ -9,11 +9,14 @@ export type GrindStoneResult = {
     readonly priorityWeight: number;
     readonly preferenceWeight: number;
 
-    readonly application: string;
-    readonly category: string;
-    readonly identifier: string;
-
     readonly description?: string;
     readonly date?: Date;
     readonly highlights?: Record<string, any>;
+} & GrindstoneCommon;
+
+export type GrindstoneCommon = {
+
+    readonly application: string;
+    readonly category: string;
+    readonly identifier: string;
 };
