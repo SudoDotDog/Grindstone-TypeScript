f5e836aa1d8ded6bba0949911bef6919bcc84ce5
WMXPY <wm@mengw.io>
2019-12-15 13:38:36 -0600

    export from index

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,11 @@
  * @description Index
  */
 
+export * from "./common/grindstone";
+export * from "./common/object-builder";
 export * from "./declare";
-export * from "./search/import";
+export * from "./search/consumer";
+export * from "./search/declare";
+export * from "./search/provider";
+export * from "./search/search";
 
